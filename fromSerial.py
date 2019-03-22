e45354947bb4b74e32fecaf55f4a80cb81790ee8
harsh2338 <43722602+harsh2338@users.noreply.github.com>
2019-03-22 19:39:01 +0530

    Need to clear the graph after every plot

--- fromSerial.py
+++ fromSerial.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import serial
-
+#need to set a1,a2,a3 values
+'''
 a1=#
 b1=#
 
@@ -11,11 +12,11 @@ b2=#
 
 a3=#
 b3=#
-
+'''
 #portname to be given
 Data=serial.Serial('com9',9600)
 while True:
-    plt.clf()
+    #plt.clf()
     while (Data.inWaiting()==0):
         pass
     coord=str(Data.readline())
@@ -26,18 +27,31 @@ while True:
     d1=int(coordarr[0])
     d2=int(coordarr[1])
     d3=int(coordarr[2])
-    #print(d1)
+    
     A = np.array ( ( [ -2*a1 + 2*a2 , -2*b1 + 2*b2 ], [ -2*a2 + 2*a3, -2*b2 + 2*b3 ] ) )
     B = np.array ( ( [ d1*d1 - d2*d2 - b1*b1 + b2*b2 - a1*a1 + a2*a2 ], [ d2*d2 - d3*d3 - b2*b2 + b3*b3 - a2*a2 + a3*a3 ] ) )
     coordinates = np.linalg.solve (A, B)
     _x = coordinates[0]
     _y = coordinates[1]
     
-    plt.pause(1)
+    '''plt.pause(1)
     plt.ylim(0,10)#will be set later
     plt.xlim(0,10)
     plt.plot(_x,_y,"bo")
     
     #plt.show()
-    
-   
+    '''
+    plt.pause(0.000000001)
+    plt.ylim(0,10)#will be set later
+    plt.xlim(0,10)
+    plt.plot(_x,_y,"bo")
+
+'''
+    plt.plot(d1,d2,"bo")
+    print(d1)
+    print(d2)
+    print(d3)
+    print('\n')
+    '''
+    plt.show()
+    continue
